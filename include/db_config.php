d8f20a508b81e27d214e18bdab2d5bc9dc013519
plays2009 <plays2009@naver.com>
2017-07-24 10:49:56 +0900

    commit test
    test

--- include/db_config.php
+++ include/db_config.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';            // 호스트 이름을 설정
 $user = 'root';                 // 최고 권한 관리자 root
-$pw = 'super815';               // root 관리자 비밀번호
+$pw = 'rentacaro';               // root 관리자 비밀번호
 $dbname = 'rentacaro';           // 메인 데이터베이스 호출
 
 $mysqli = new mysqli($host, $user, $pw, $dbname);
