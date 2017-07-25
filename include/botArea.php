4dbced7d450f9c953f52d22c2f77d436398cf0d4
hamseul <sl930905@naver.com>
2017-07-25 15:22:52 +0900

    bot화면 출력

--- include/botArea.php
+++ include/botArea.php
@@ -1,20 +1,18 @@
 <?php
 // 상단 메인 이미지 출력 위한 데이터베이스 호출
-$cntQuery = "SELECT count(idx) FROM main_images";
+$cntQuery = "SELECT count(idx) FROM bot_image";
 $cntResult = $mysqli->query($cntQuery);
 $cntData = mysqli_fetch_array($cntResult);
-for ($cnt = 0; $cnt < $cntData[0]; $cnt++)
+$BotQuery = "SELECT * FROM bot_image";
+$botResult = $mysqli->query($BotQuery);
+while ($BotData = mysqli_fetch_array($botResult))
 {
-  $topQuery = "SELECT * FROM main_images WHERE pos = $cnt+1";
-  $topResult = $mysqli->query($topQuery);
-  $topData = mysqli_fetch_array($topResult);
-
-  if ($topData['image'])
+  if ($BotData['image'])
   {
 ?>
 
 <div class="" style="width: 960px; height: auto">
-  <img src="img/<?=$topData['image']?>" style="width:960px"></img>
+  <img src="img/<?=$BotData['image']?>" style="width:960px"></img>
 </div>
 
 <?php  }  else  { ?>
